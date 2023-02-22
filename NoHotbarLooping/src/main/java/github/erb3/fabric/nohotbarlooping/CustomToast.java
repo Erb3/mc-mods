d21a952d20714ab14a2d2886282e7f701a2b9e3f
Erlend <49862976+Erb3@users.noreply.github.com>
2023-02-22 07:49:37 +0100

    feat (NoHotbarLooping): Add toasts

---
+++ NoHotbarLooping/src/main/java/github/erb3/fabric/nohotbarlooping/CustomToast.java
@@ -0,0 +1,59 @@
+package github.erb3.fabric.nohotbarlooping;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.toast.Toast;
+import net.minecraft.client.toast.ToastManager;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import org.jetbrains.annotations.NotNull;
+
+import java.awt.*;
+
+public class CustomToast implements Toast {
+
+    // Inspired by Meteor-client/MeteorToast
+
+    public static final int titleColor = Color.WHITE.getRGB();
+    public static final int textColor = Color.LIGHT_GRAY.getRGB();
+
+    private ItemStack icon;
+    private Text title, text;
+    private boolean timeStarted = false;
+    private long start, duration;
+
+    public CustomToast(@NotNull Item item, @NotNull String title, @NotNull String text) {
+        this.icon = item.getDefaultStack();
+        this.title = Text.literal(title).setStyle(Style.EMPTY.withColor(titleColor));
+        this.text = Text.literal(text).setStyle(Style.EMPTY.withColor(textColor));
+        this.duration = 6000;
+    }
+
+
+
+    @Override
+    public Visibility draw(MatrixStack matrices, ToastManager manager, long currentTime) {
+        if (!timeStarted) {
+            start = currentTime;
+            timeStarted = true;
+        }
+
+        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
+        RenderSystem.setShaderTexture(0, TEXTURE);
+        RenderSystem.setShaderColor(1, 1, 1, 1);
+        manager.drawTexture(matrices, 0, 0, 0, 0, getWidth(), getHeight());
+
+        int x = 28;
+        MinecraftClient mc = MinecraftClient.getInstance();
+
+        mc.textRenderer.draw(matrices, title, x, 7, titleColor);
+        mc.textRenderer.draw(matrices, text, x, 18, textColor);
+        mc.getItemRenderer().renderInGui(icon, 8, 8);
+
+        return currentTime - start >= duration ? Toast.Visibility.HIDE : Toast.Visibility.SHOW;
+    }
+}
--- NoHotbarLooping/src/main/java/github/erb3/fabric/nohotbarlooping/Nohotbarlooping.java
+++ NoHotbarLooping/src/main/java/github/erb3/fabric/nohotbarlooping/Nohotbarlooping.java
@@ -3,9 +3,16 @@ package github.erb3.fabric.nohotbarlooping;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.block.Material;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.toast.Toast;
+import net.minecraft.client.toast.ToastManager;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 import org.slf4j.Logger;
@@ -14,13 +21,17 @@ import org.slf4j.LoggerFactory;
 public class Nohotbarlooping implements ClientModInitializer {
     public static Logger LOGGER = LoggerFactory.getLogger("NoHotbarLooping");
     public static MinecraftClient client;
-    public static boolean enabled = true;
     private static KeyBinding keyBind;
+    private static ToastManager toaster;
+
+    public static boolean enabled = true;
+
 
     @Override
     public void onInitializeClient() {
         LOGGER.info("Hello from NoHotbarLooping main class!");
         client = MinecraftClient.getInstance();
+        toaster = client.getToastManager();
 
         keyBind = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "nohotbarlooping.keybind.name",
@@ -32,8 +43,26 @@ public class Nohotbarlooping implements ClientModInitializer {
         ClientTickEvents.END_CLIENT_TICK.register((client) -> {
             while (keyBind.wasPressed()) {
                 enabled = !enabled;
+                renderToast(enabled);
             }
         });
+    }
+
+    private static void renderToast(boolean enabled) {
+        Item item;
+        String title;
+
+        if (enabled) {
+            item = Items.REDSTONE_BLOCK;
+            title = "Hotbar looping disabled";
+        } else {
+            item = Items.EMERALD_BLOCK;
+            title = "Hotbar looping enabled";
+        }
+
+        Toast toast = new CustomToast(item, title, "- NoHotbarLooping");
+        toaster.clear();
+        toaster.add(toast);
     }
 
     public static Text translate(String key) {
