5c18865a208426bd7590e27659a91b5c496571fb
Erlend <49862976+Erb3@users.noreply.github.com>
2023-03-01 09:08:57 +0100

    feat (BeePeripherals): Document the sign integration

--- BeePeripherals/src/main/java/github/erb3/fabric/beeperipherals/integrations/SignPeripheral.java
+++ BeePeripherals/src/main/java/github/erb3/fabric/beeperipherals/integrations/SignPeripheral.java
@@ -29,6 +29,13 @@ public class SignPeripheral implements GenericPeripheral {
         signWorld.updateListeners(signPos, signState, signState, Block.NOTIFY_ALL);
     }
 
+    /**
+     * A function which will replace the sign text with the incoming four arguments.
+     * If you only want to change the first two lines, and last two empty, just do two args instead.
+     * @param sign The sign you want to replace text on.
+     * @param args The new sign text, four string arguments, default is empty string.
+     * @throws LuaException Anything can happen, so we throw lua exceptions.
+     */
     @LuaFunction(mainThread = true)
     public static void setSignText(SignBlockEntity sign, IArguments args) throws LuaException {
         for (int i = 0; i < 4; i++) {
@@ -39,6 +46,12 @@ public class SignPeripheral implements GenericPeripheral {
         blockUpdate(sign);
     }
 
+    /**
+     * A function which will edit sign text, essentially setSignText just that default is to not edit the line.
+     * @param sign The sign you want to edit text on.
+     * @param args The new sign text, four string arguments, empty string will skip the line.
+     * @throws LuaException Anything can happen in lua!
+     */
     @LuaFunction(mainThread = true)
     public static void editSignText(SignBlockEntity sign, IArguments args) throws  LuaException {
         for (int i = 0; i < 4; i++) {
@@ -49,6 +62,13 @@ public class SignPeripheral implements GenericPeripheral {
         blockUpdate(sign);
     }
 
+    /**
+     * A function which will set the text of a specific line.
+     * @param sign The sign you want to edit a line on.
+     * @param line The line number (0 -> 3).
+     * @param newLine The new text you want on the line.
+     * @throws LuaException Anything can happen!
+     */
     @LuaFunction(mainThread = true)
     public static void setSignLine(SignBlockEntity sign, int line, String newLine) throws LuaException {
         if (!(line >= 0 && line < 4)) {
@@ -59,6 +79,12 @@ public class SignPeripheral implements GenericPeripheral {
         blockUpdate(sign);
     }
 
+
+    /**
+     * Function to get all the text on the sign.
+     * @param sign The sign you want to read text off.
+     * @return A table containing strings of the different lines.
+     */
     // Main thread due to world related
     @LuaFunction(mainThread = true)
     public static ArrayList<String> getSignText(SignBlockEntity sign) {
@@ -71,6 +97,13 @@ public class SignPeripheral implements GenericPeripheral {
         return lines;
     }
 
+    /**
+     * Gets the text on the specified line.
+     * @param sign The sign you want to read a line off.
+     * @param line The line number you want to read (0 -> 3).
+     * @return A string with the text on that line.
+     * @throws LuaException Throws if you have bad arguments
+     */
     @LuaFunction(mainThread = true)
     public static String getSignLine(SignBlockEntity sign, int line) throws LuaException {
         if (!(line >= 0 && line < 4)) {
